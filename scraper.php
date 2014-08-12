2a9c7b9c4cfbed9f814c0e4dfa8e4da2799a57ed
sabas <sabas88@gmail.com>
2014-08-12 09:03:49 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,26 +1,36 @@
-<?
+<?php
 // This is a template for a PHP scraper on Morph (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+ require 'scraperwiki.php';
+ require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+ $html = scraperwiki::scrape("http://www.agid.gov.it/catalogo-nazionale-programmi-riusabili");
 //
 // // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+$dom = new simple_html_dom();
+$dom->load($html);
+$tab=$dom->find("table.views-table tbody");
 
-// You don't have to do things with the ScraperWiki library. You can use whatever is installed
-// on Morph for PHP (See https://github.com/openaustralia/morph-docker-php) and all that matters
-// is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-// has at least a table called data.
+
+foreach($tab as $row)
+{
+ $row=$table->find("tr td");
+ $ID=trim($row[0]);
+ $Titolo=trim($row[1]);
+ $Anno=trim($row[2]);
+ $Amministrazione=trim($row[3]);
+ $Scheda_Applicazione=trim($row[4]);
+ 
+ $record = array(
+   'ID' => $ID,
+   'Titolo' => $Titolo,
+   'Anno' => $Anno,
+   'Amministrazione' => $Amministrazione,
+   'Scheda Applicazione' => $Scheda_Applicazione
+ );
+ 
+scraperwiki::save_sqlite(array('ID'), $record); 
+}
 ?>
