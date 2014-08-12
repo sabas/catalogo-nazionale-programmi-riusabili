2337ed94b6a09d85e381e120acc9333fd9c61bdd
sabas <sabas88@gmail.com>
2014-08-12 09:35:21 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,14 +1,9 @@
 <?php
-// This is a template for a PHP scraper on Morph (https://morph.io)
-// including some code snippets below that you should find helpful
-
  require 'scraperwiki.php';
  require 'simple_html_dom.php';
-//
-// // Read in a page
+ 
  $html = scraperwiki::scrape("http://www.agid.gov.it/catalogo-nazionale-programmi-riusabili");
-//
-// // Find something on the page using css selectors
+
 $dom = new simple_html_dom();
 $dom->load($html);
 $tab=$dom->find("table.views-table");
@@ -16,11 +11,12 @@ $tab=$tab[0]->find("tbody tr");
 foreach($tab as $row)
 {
  $row=$row->find("td");
+ if (!isset($row[0])) continue;
  $ID=trim($row[0]->plaintext);
- $Titolo=trim($row[1]->plaintext);
- $Anno=trim($row[2]->plaintext);
- $Amministrazione=trim($row[3]->plaintext);
- $Scheda_Applicazione=trim($row[4]->plaintext);
+ $Titolo=trim(utf8_decode($row[1]->plaintext));
+ $Anno=trim(utf8_decode($row[2]->plaintext));
+ $Amministrazione=trim(utf8_decode($row[3]->plaintext));
+ $Scheda_Applicazione=trim(utf8_decode($row[4]->plaintext));
  $record = array(
    'ID' => $ID,
    'Titolo' => $Titolo,
@@ -28,7 +24,6 @@ foreach($tab as $row)
    'Amministrazione' => $Amministrazione,
    'Scheda Applicazione' => $Scheda_Applicazione
  );
- var_dump($record);
-//scraperwiki::save_sqlite(array('ID'), $record); 
+ scraperwiki::save_sqlite(array('ID'), $record); 
 }
 ?>
