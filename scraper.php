e1a0d015286dcfaae695c33482bfcc75402ba5b6
sabas <sabas88@gmail.com>
2014-09-12 16:14:42 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,8 +1,8 @@
 <?php
- require 'scraperwiki.php';
- require 'simple_html_dom.php';
+require 'scraperwiki.php';
+require 'simple_html_dom.php';
  
- $html = scraperwiki::scrape("http://www.agid.gov.it/catalogo-nazionale-programmi-riusabili");
+$html = scraperwiki::scrape("http://www.agid.gov.it/catalogo-nazionale-programmi-riusabili");
 
 $dom = new simple_html_dom();
 $dom->load($html);
@@ -10,20 +10,21 @@ $tab=$dom->find("table.views-table");
 $tab=$tab[0]->find("tbody tr");
 foreach($tab as $row)
 {
- $row=$row->find("td");
- if (!isset($row[0])) continue;
- $ID=trim($row[0]->plaintext);
- $Titolo=trim(utf8_decode($row[1]->plaintext));
- $Anno=trim(utf8_decode($row[2]->plaintext));
- $Amministrazione=trim(utf8_decode($row[3]->plaintext));
- $Scheda_Applicazione=trim(utf8_decode($row[4]->plaintext));
- $record = array(
-   'ID' => $ID,
-   'Titolo' => $Titolo,
-   'Anno' => $Anno,
-   'Amministrazione' => $Amministrazione,
-   'Scheda_Applicazione' => $Scheda_Applicazione
- );
- scraperwiki::save_sqlite(array('ID'), $record); 
+      $row=$row->find("td");
+      if (!isset($row[0])) continue;
+      $ID=trim($row[0]->plaintext);
+      $Titolo=trim(utf8_decode($row[1]->plaintext));
+      $Anno=trim(utf8_decode($row[2]->plaintext));
+      $Amministrazione=trim(utf8_decode($row[3]->plaintext));
+      $Scheda_Applicazione=trim(utf8_decode($row[4]->plaintext));
+      $record = array(
+        'id' => $ID,
+        'titolo' => $Titolo,
+        'anno' => $Anno,
+        'amministrazione' => $Amministrazione,
+        'scheda_applicazione' => $Scheda_Applicazione
+      );
+      $arr[]=$record;
 }
+scraperwiki::save_sqlite(array('id'), $arr);
 ?>
